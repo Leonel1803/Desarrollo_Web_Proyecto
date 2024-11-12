ad6d517826179e34fe48240c83d2393af4a5d715
Leon Rivera <leonrivera2003@gmail.com>
2024-11-12 11:18:39 -0600

    Creación de notas implementado

---
+++ views/js/image_util.js
@@ -0,0 +1,45 @@
+//Subida de portada
+const dropArea = document.getElementById('dropArea');
+const preview = document.getElementById('preview');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, e => e.preventDefault());
+    dropArea.addEventListener(eventName, e => e.stopPropagation());
+});
+
+// Cambiar el estilo cuando se arrastra sobre el área
+['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, () => {
+        dropArea.classList.add('dragover');
+    });
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, () => {
+        dropArea.classList.remove('dragover');
+    });
+});
+
+// Manejar el evento de soltar
+dropArea.addEventListener('drop', e => {
+    const files = e.dataTransfer.files;
+    if (files.length) {
+        const file = files[0];
+
+        // Verifica si el archivo es una imagen
+        if (file.type.startsWith('image/')) {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+
+            reader.onload = function(event) {
+                const img = document.createElement('img');
+                img.id = 'notePortrait'
+                img.src = event.target.result;
+                preview.innerHTML = '';
+                preview.appendChild(img);
+            };
+        } else {
+            alert("Por favor, suelta solo imágenes.");
+        }
+    }
+});
--- views/js/notes.js
+++ views/js/notes.js
@@ -1,9 +1,30 @@
+const newNoteTitle = document.getElementById('new-note-input');
+const newNoteDescription = document.getElementById('note-description-input');
+const newNotePortrait = document.getElementById('preview');
+
+let actualCategoryEdit = '';
+
 const createNoteInput = (input, type, id, classes) => {
     input.type = type;
     input.id = id;
     input.className = classes;
 
     return input;
+}
+
+const newNoteButton = (notesDiv) => {
+    notesDiv.innerHTML = `
+        <div onclick="getActualCategory(event)" data-bs-toggle="modal" data-bs-target="#new-note" class="card turn-green me-3" style="width: 18rem;">
+            <img src="./assets/nueva-nota.png" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Nueva Nota</h5>
+                <p class="card-text">
+                    ¡Crea una nueva nota con el 
+                    contenido que tu quieras!
+                </p>
+            </div>
+        </div>
+    `
 }
 
 const createCategory = () => {
@@ -15,7 +36,7 @@ const createCategory = () => {
 
     noteNameInput.focus(); //Activamos el input automáticamente para que el susuario empiece a escribir
 
-    noteNameInput.addEventListener('blur', () => { //Si se hace click fuera dle input, este se borra
+    noteNameInput.addEventListener('blur', () => { //Si se hace click fuera del input, este se borra
         categories.removeChild(noteNameInput);
         return;
     });
@@ -24,20 +45,68 @@ const createCategory = () => {
         if (event.key === 'Enter') { // Verificamos si la tecla presionada es 'Enter'
             // Crear el nuevo div con el h5
             const newCategory = document.createElement('div');
-            newCategory.id = noteNameInput.value;
+            newCategory.id = noteNameInput.value.replace(/ /g, "-");
+            newCategory.style = 'height: 480px';
 
             const h5 = document.createElement('h5');
             h5.innerText = noteNameInput.value; // Asignar el valor del input al h5
 
             const notesDiv = document.createElement('div');
-            notesDiv.id = 'notes' + noteNameInput.value;
+            notesDiv.className = 'd-flex'
+            notesDiv.id = ('notes-' + noteNameInput.value).replace(/ /g, "-");
 
             newCategory.appendChild(h5);
             newCategory.appendChild(notesDiv);
             categories.appendChild(newCategory); // Agregamos el nuevo div a categories
+
+            newNoteButton(notesDiv);
 
             categories.removeChild(noteNameInput); // Removemos el input
             return;
         }
     });
 };
+
+const getActualCategory = (event) => {
+    actualCategoryEdit = event.currentTarget.parentElement.id;
+}
+
+const noteToHTML = (note) => {
+    return `
+        <div class="card turn-green me-3" style="width: 18rem;">
+            <img src=${note.imageURL} class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${note.title}</h5>
+                <p class="card-text">
+                    ${note.description}
+                </p>
+            </div>
+        </div>
+    `
+}
+
+const createNote = () => {
+    const portrait = document.getElementById('notePortrait');
+
+    const newNote = {
+        title: newNoteTitle.value,
+        description: newNoteDescription.value,
+        imageURL: portrait.src
+    }
+
+    const newNoteElement = document.createElement('div');
+    newNoteElement.innerHTML = noteToHTML(newNote).trim();
+
+    const actualCategory = document.getElementById(actualCategoryEdit);
+    const firstChild = actualCategory.children[1];
+
+    baseElement.insertBefore(newNoteElement.firstChild, firstChild);
+
+    deleteActualInfo();
+}
+
+const deleteActualInfo = () => {
+    newNoteTitle.value = '';
+    newNoteDescription.value = '';
+    newNotePortrait.innerHTML = '';
+}
